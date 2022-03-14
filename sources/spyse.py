c7c2451422f5363d7f1615b5d0a7630410f208ef
Felipe Caon <felipecaon@gmail.com>
2022-03-13 23:27:51 -0300

    dot

--- sources/spyse.py
+++ sources/spyse.py
@@ -14,7 +14,7 @@ class Spyse:
 
             firefox_header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"}
 
-            response = requests.get(f"https://spyse.com/search?target=domain&query={domain_name}&search_params=%5B%7B%22is_subdomain%22%3A%7B%22operator%22%3A%22eq%22,%22value%22%3A%22true%22%7D%7D%5D", timeout=15, headers=firefox_header)
+            response = requests.get(f"https://spyse.com/search?target=domain&query=.{domain_name}&search_params=%5B%7B%22is_subdomain%22%3A%7B%22operator%22%3A%22eq%22,%22value%22%3A%22true%22%7D%7D%5D", timeout=15, headers=firefox_header)
 
             subdomains = 0 
             tree = html.fromstring(response.content)
